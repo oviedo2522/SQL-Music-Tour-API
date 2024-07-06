4caf354ac7fc7be760be47d5d504774d9f1ffc75
oviedo2522 <oviedo2522@gmail.com>
2024-07-06 13:35:37 -0700

    new folder

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -14,7 +16,20 @@ app.get('/', (req, res) => {
     })
 })
 
+// Sequelize Connection 
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
+
+

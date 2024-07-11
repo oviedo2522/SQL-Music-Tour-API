4a4605cae1a629f0cb0a47b281c1aa61bd756f9e
oviedo2522 <oviedo2522@gmail.com>
2024-07-10 20:20:36 -0700

    new folders

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
-
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -16,11 +16,13 @@ app.get('/', (req, res) => {
     })
 })
 
+// CONTROLLERS  
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
-
 
 
